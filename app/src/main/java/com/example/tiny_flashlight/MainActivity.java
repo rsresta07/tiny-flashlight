e1f62665ebb9be3a8ff11943bd15286bd2a2d636
Rameshwor Shrestha <r.sresta07@gmail.com>
2024-06-30 22:13:41 +0545

    completed tiny-falshlight

--- app/src/main/java/com/example/tiny_flashlight/MainActivity.java
+++ app/src/main/java/com/example/tiny_flashlight/MainActivity.java
@@ -1,24 +1,72 @@
 package com.example.tiny_flashlight;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton toggleButton;
+    boolean hasCameraFlash = false;
+    boolean flashOn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        toggleButton = findViewById(R.id.toggleButton);
+
+        hasCameraFlash = getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (hasCameraFlash) {
+                    if (flashOn) {
+                        flashOn = false;
+                        toggleButton.setImageResource(R.drawable.off_button);
+                        try {
+                            flashLightOff();
+                        } catch (CameraAccessException e) {
+                            throw new RuntimeException(e);
+                        }
+                    } else {
+                        flashOn = true;
+                        toggleButton.setImageResource(R.drawable.on_button);
+                        try {
+                            flashLightOn();
+                        } catch (CameraAccessException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "No flash available on your device", Toast.LENGTH_LONG).show();
+                }
+            }
         });
+    }
+
+    private void flashLightOn() throws CameraAccessException {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        assert cameraManager != null;
+        String cameraId = cameraManager.getCameraIdList()[0];
+        cameraManager.setTorchMode(cameraId, true);
+        Toast.makeText(MainActivity.this, "FlashLight is ON", Toast.LENGTH_SHORT).show();
+    }
+
+    private void flashLightOff() throws CameraAccessException {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        assert cameraManager != null;
+        String cameraId = cameraManager.getCameraIdList()[0];
+        cameraManager.setTorchMode(cameraId, false);
+        Toast.makeText(MainActivity.this, "FlashLight is OFF", Toast.LENGTH_SHORT).show();
     }
 }
